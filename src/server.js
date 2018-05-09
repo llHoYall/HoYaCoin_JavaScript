bdeba51146581fe62ff1cb808db232f9ac7fe8be
llHoYall <hoya128@gmail.com>
2018-05-09 21:25:26 +0900

    Update server
    - Add route: GET /blocks
    - Add route: POST /blocks

--- src/server.js
+++ src/server.js
@@ -12,4 +12,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 
+app.get("/blocks", (req, res) => {
+  res.send(getBlockchain());
+});
+
+app.post("/blocks", (req, res) => {
+  const {
+    body: { data }
+  } = req;
+  const new_block = createNewBlock(data);
+  res.send(new_block);
+});
+
 app.listen(PORT, () => console.log(`HoYaCoin server running on ${PORT}`));

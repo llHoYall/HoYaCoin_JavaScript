74e17a14bbe2f463c790b0dba4c21308772b3051
llHoYall <hoya128@gmail.com>
2018-05-11 21:42:43 +0900

    Mining coins

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,12 +1,18 @@
 const CryptoJS = require("crypto-js");
+const hexToBinary = require("hex-to-binary");
+
+const BLOCK_GENERATION_INTERVAL = 10;
+const DIFFICULTY_ADJUSTMENT_INTERVAL = 10;
 
 class Block {
-  constructor(index, hash, prev_hash, timestamp, data) {
+  constructor(index, hash, prev_hash, timestamp, data, difficulty, nonce) {
     this.index = index;
     this.hash = hash;
     this.prev_hash = prev_hash;
     this.timestamp = timestamp;
     this.data = data;
+    this.difficulty = difficulty;
+    this.nonce = nonce;
   }
 }
 
@@ -15,48 +21,113 @@ const genesisBlock = new Block(
   "4DF9140CE289EF9596AFA964C082633D1AC84F2C7EA83F2C8E70FDD6160B8FC1",
   null,
   1525783768553,
-  "This is a genesis block."
+  "This is a genesis block.",
+  0,
+  0
 );
 
 let blockchain = [genesisBlock];
 
-const getLastBlock = () => blockchain[blockchain.length - 1];
+const getNewestBlock = () => blockchain[blockchain.length - 1];
 
 const getBlockchain = () => blockchain;
 
 const getTimestamp = () => new Date().getTime() / 1000;
 
-const createHash = (index, prev_hash, timestamp, data) =>
+const createHash = (index, prev_hash, timestamp, data, difficulty, nonce) =>
   CryptoJS.SHA256(
-    index + prev_hash + timestamp + JSON.stringify(data)
+    index + prev_hash + timestamp + JSON.stringify(data) + difficulty + nonce
   ).toString();
 
 const createNewBlock = data => {
-  const prev_block = getLastBlock();
+  const prev_block = getNewestBlock();
   const new_block_index = prev_block.index + 1;
   const new_timestamp = getTimestamp();
-  const new_hash = createHash(
-    new_block_index,
-    prev_block.hash,
-    new_timestamp,
-    data
-  );
+  const difficulty = findDifficulty();
   const new_block = new Block(
     new_block_index,
-    new_hash,
     prev_block.hash,
     new_timestamp,
-    data
+    data,
+    difficulty
   );
   addBlockToChain(new_block);
+  require("./p2p").broadcastNewBlock();
   return new_block;
 };
 
-const getBlockHash = block =>
-  createHash(block.index, block.prev_hash, block.timestamp, block.data);
+const findDifficulty = () => {
+  const newest_block = getNewestBlock();
+  if (
+    newest_block.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 &&
+    newest_block.index !== 0
+  ) {
+    return calculateNewDifficulty(newest_block, getBlockchain());
+  } else {
+    return newest_block.difficulty;
+  }
+};
 
-const isNewBlockValid = (candidate_block, latest_block) => {
-  if (!isNewStructureValid(candidate_block)) {
+const calculateNewDifficulty = (newest_block, blockchain) => {
+  const last_calculated_block =
+    blockchain[blockchain.lenth - DIFFICULTY_ADJUSTMENT_INTERVAL];
+  const timeExpected =
+    BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
+  const timeTaken = newest_block.timestamp - last_calculated_block.timestamp;
+  if (timeTaken < timeExpected / 2) {
+    return last_calculated_block.difficulty + 1;
+  } else if (timeTaken > timeExpected * 2) {
+    return last_calculated_block.difficulty - 1;
+  } else {
+    return last_calculated_block.difficulty;
+  }
+};
+
+const findBlock = (index, prev_hash, timestamp, data, difficulty) => {
+  let nonce = 0;
+  while (true) {
+    const hash = createHash(
+      index,
+      prev_hash,
+      timestamp,
+      data,
+      difficulty,
+      nonce
+    );
+    if (hashMatchesDifficulty(hash, difficulty)) {
+      return new Block(
+        index,
+        hash,
+        prev_hash,
+        timestamp,
+        data,
+        difficulty,
+        nonce
+      );
+    } else {
+      nonce++;
+    }
+  }
+};
+
+const hashMatchesDifficulty = (hash, difficulty) => {
+  const hash_in_binary = hexToBinary(hash);
+  const required_zeros = "0".repeat(difficulty);
+  return hash_in_binary.startsWith(required_zeros);
+};
+
+const getBlockHash = block =>
+  createHash(
+    block.index,
+    block.prev_hash,
+    block.timestamp,
+    block.data,
+    block.difficulty,
+    block.nonce
+  );
+
+const isBlockValid = (candidate_block, latest_block) => {
+  if (!isBlockStructureValid(candidate_block)) {
     console.log("The candidate block structure is not valid");
     return false;
   } else if (candidate_block.index !== latest_block.index + 1) {
@@ -74,7 +145,7 @@ const isNewBlockValid = (candidate_block, latest_block) => {
   return true;
 };
 
-const isNewStructureValid = block => {
+const isBlockStructureValid = block => {
   return (
     typeof block.index === "number" &&
     typeof block.hash === "string" &&
@@ -95,7 +166,7 @@ const isChainValid = candidate_chain => {
     return false;
   }
   for (let i = 1; i < candidate_chain.length; i++) {
-    if (!isNewBlockValid(candidate_chain[i], candidate_chain[i - 1])) {
+    if (!isBlockValid(candidate_chain[i], candidate_chain[i - 1])) {
       return false;
     }
   }
@@ -114,7 +185,7 @@ const replaceChain = candidate_chain => {
 };
 
 const addBlockToChain = candidate_block => {
-  if (isNewBlockValid(candidate_block, getLastBlock())) {
+  if (isBlockValid(candidate_block, getNewestBlock())) {
     getBlockchain().push(candidate_block);
     return true;
   }
@@ -123,6 +194,9 @@ const addBlockToChain = candidate_block => {
 
 module.exports = {
   getBlockchain,
-  getLastBlock,
-  createNewBlock
+  getNewestBlock,
+  createNewBlock,
+  isBlockStructureValid,
+  addBlockToChain,
+  replaceChain
 };
--- src/p2p.js
+++ src/p2p.js
@@ -1,7 +1,13 @@
 const WebSockets = require("ws");
 const Blokchain = require("./blockchain");
 
-const { getLastBlock } = Blokchain;
+const {
+  getBlockchain,
+  getNewestBlock,
+  isBlockStructureValid,
+  addBlockToChain,
+  replaceChain
+} = Blokchain;
 
 // Message Types
 const GET_LATEST = "GET_LATEST";
@@ -64,16 +70,59 @@ const handleSocketMessage = ws => {
     if (message === null) {
       return;
     }
-    console.log(message);
     switch (message.type) {
       case GET_LATEST:
-        sendMessage(ws, getLastBlock());
+        sendMessage(ws, responseLatest());
+        break;
+      case GET_ALL:
+        sendMessage(ws, responseAll());
+        break;
+      case BLOCKCHAIN_RSP:
+        const received_block = message.data;
+        if (received_block === null) {
+          break;
+        }
+        handleBlockchainResponse(received_block);
         break;
     }
   });
 };
 
 const sendMessage = (ws, message) => ws.send(JSON.stringify(message));
+
+const sendMessageToAll = message => {
+  sockets.forEach(ws => sendMessage(ws, message));
+};
+
+const handleBlockchainResponse = received_block => {
+  if (received_block.length === 0) {
+    console.log("Receive an empty block");
+    return;
+  }
+  const latest_received_block = received_block[received_block.length - 1];
+  if (!isBlockStructureValid(latest_received_block)) {
+    console.log("The block structure of the received block is not valid");
+    return;
+  }
+  const newest_block = getNewestBlock();
+  if (latest_received_block.index > newest_block.index) {
+    if (latest_received_block.prev_hash === newest_block.hash) {
+      if (addBlockToChain(latest_received_block)) {
+        broadcastNewBlock();
+      }
+    } else if (received_block.length === 1) {
+      sendMessageToAll(getAll());
+    } else {
+      replaceChain(received_block);
+    }
+  }
+};
+
+const responseLatest = () => blockchainResponse([getNewestBlock()]);
+
+const responseAll = () => blockchainResponse(getBlockchain());
+
+const broadcastNewBlock = () => sendMessageToAll(responseLatest());
 
 const handleSocketError = ws => {
   const closeSocketConnection = ws => {
@@ -93,5 +142,6 @@ const connectToPeers = new_peer => {
 
 module.exports = {
   startP2PServer,
-  connectToPeers
+  connectToPeers,
+  broadcastNewBlock
 };

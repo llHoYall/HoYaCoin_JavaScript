787a470a000c2a14a8471fa8a82e3e461c09cac3
llHoYall <hoya128@gmail.com>
2018-06-07 22:12:49 +0900

    Update project: Coin Explorer
    - Getting blockchain data.

--- explorer/src/components/app/app-container.js
+++ explorer/src/components/app/app-container.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { injectGlobal } from "styled-components";
 import reset from "styled-reset";
 import typography from "../../typography";
+import axios from "axios";
 import AppPresenter from "./app-presenter";
+import { API_URL } from "../../constants";
 
 const baseStyles = () => injectGlobal`
     ${reset};
@@ -13,10 +15,27 @@ const baseStyles = () => injectGlobal`
 `;
 
 class AppContainer extends Component {
+  state = {
+    isLoading: true
+  };
+
+  componentDidMount = () => {
+    this._getData();
+  };
+
   render() {
     baseStyles();
-    return <AppPresenter />;
+    return <AppPresenter {...this.state} />;
   }
+
+  _getData = async () => {
+    const request = await axios.get(`${API_URL}/blocks`);
+    const blocks = request.data;
+    this.setState({
+      blocks,
+      isLoading: false
+    });
+  };
 }
 
 export default AppContainer;
